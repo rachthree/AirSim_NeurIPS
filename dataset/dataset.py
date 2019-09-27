03fa5cdcfe2a78b39e21800e4c43bc08d3a6ae88
rachpag <rachpag@users.noreply.github.com>
2019-09-26 21:51:16 -0700

    Initial Commit

---
+++ dataset/dataset.py
@@ -0,0 +1,12 @@
+import airsimneurips as airsim
+import numpy as np
+
+def generate_dataset(save_location, n_images):
+    # TODO: Place gate with random poses given ranges - x y z rx ry rz
+    # TODO: Place drone in relation to gate - x y z rx ry rz
+    # TODO: Take RBG Image
+    # TODO: Get location of gate corners and flyable region
+    # TODO: Project points to image
+    # TODO: Generate Mask
+    # TODO: Save PNG
+    pass
